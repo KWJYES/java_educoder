ae093d5c451dbe73ea7306ebd34a36197e43f20b
WKJYES <iwkj@foxmail.com>
2023-02-27 19:06:06 +0800

    完成了"Java语言基础综合实训（1）"第一关

---
+++ src/educoder/test1/L1/HelloWorld.java
@@ -0,0 +1,10 @@
+package educoder.test1.L1;
+
+public class HelloWorld{
+    public static void main(String[] args){
+        /********* Begin *********/
+        String love = "www.educoder.net";
+        System.out.println(love);
+        /********* End *********/
+    }
+}
